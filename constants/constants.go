652bdeff1eb9833a9085f66682473fd3d382b2a6
Vegard Johansen <vegardj01@gmail.com>
2023-03-02 09:42:42 +0100

    Added constants for third party URLs

--- constants/constants.go
+++ constants/constants.go
@@ -5,3 +5,6 @@ const DEFAULT_PATH = "/unisearcher/"
 const UINFO_PATH = DEFAULT_PATH + "v1/uniinfo/"
 const UNEIGHBOUR_PATH = DEFAULT_PATH + "v1/neighbourunis/"
 const DIAG_PATH = DEFAULT_PATH + "v1/diag/"
+
+const UNI_URL = "http://universities.hipolabs.com/"
+const COUNTRIES_URL = "https://restcountries.com/"
