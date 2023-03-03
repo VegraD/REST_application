582dbed01899e9ee679e316dff2e09a6626ef883
Vegard Johansen <vegardj01@gmail.com>
2023-03-03 11:32:07 +0100

    Function to convert from two structs into one.

---
+++ convert/convert.go
@@ -0,0 +1,19 @@
+package convert
+
+import (
+	"Assignment-1/structs"
+)
+
+func ToUniAndCountry(country structs.Country, uni structs.University) structs.UniAndCountry {
+	var newUni structs.UniAndCountry
+
+	newUni.Name = uni.Name
+	newUni.Country = uni.Country
+	newUni.Isocode = uni.AlphaTwoCode
+	newUni.Webpages = uni.WebPages
+	newUni.Languages = country.Languages
+	newUni.Map = country.Maps["openStreetMaps"]
+	//TODO: Error handling
+
+	return newUni
+}
